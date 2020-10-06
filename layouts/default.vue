d72697e8e22213c2af4ffa4def390afc41ef0da6
Dom Bavetta <33183744+dombavetta@users.noreply.github.com>
2020-10-06 11:48:05 -0500

    Project scaffold

---
+++ layouts/default.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="container">
+    <CThemeProvider>
+      <CColorModeProvider>
+        <CBox font-family="body" as="main">
+          <CReset />
+          <Nuxt />
+        </CBox>
+      </CColorModeProvider>
+    </CThemeProvider>
+  </div>
+</template>
+<script lang="ts">
+import { defineComponent } from '@nuxtjs/composition-api';
+
+export default defineComponent({
+  setup() {
+    return {};
+  },
+});
+</script>
